127875223d148549dad307d57103b556040d7af4
Jakub Kudzia <jakub.kudzia@outlook.com>
2016-08-08 14:46:24 +0200

    VFS-2293 - code improvements according to PR

--- artifacts/pull_artifact.py
+++ artifacts/pull_artifact.py
@@ -1,13 +1,35 @@
 #! /usr/bin/env python
+"""
+Pulls build artifact from external repo.
 
+Run the script with -h flag to learn about script's running options.
+"""
 import argparse
 import os
-from paramiko import SSHClient, AutoAddPolicy, SSHException
+from paramiko import SSHClient, AutoAddPolicy
 from scp import SCPClient, SCPException
 
 ARTIFACTS_DIR = 'artifacts'
 ARTIFACTS_EXT = '.tar.gz'
 DEFAULT_BRANCH = 'develop'
+
+
+def download_artifact(ssh, plan, branch=DEFAULT_BRANCH):
+    """
+    Downloads artifact from repo. If branch  argument is not passed, downloads
+    artifact from DEFAULT_BRANCH
+    :param ssh: sshclient with opened connection
+    :type ssh: paramiko.SSHClient
+    :param plan: name of current bamboo plan
+    :type plan: str
+    :param branch: name of current git branch
+    :type branch: str
+    :return None
+    """
+    with SCPClient(ssh.get_transport()) as scp:
+        scp.get(os.path.join(ARTIFACTS_DIR, plan, branch + ARTIFACTS_EXT),
+                local_path=args.plan.replace("-", '_') + ARTIFACTS_EXT)
+
 
 parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -17,47 +39,54 @@ parser.add_argument(
         '--hostname', '-hn',
         action='store',
         help='Hostname of artifacts repository',
-        dest='hostname')
+        dest='hostname',
+        required=True)
 
 parser.add_argument(
         '--port', '-p',
         action='store',
         type=int,
         help='SSH port to connect to',
-        dest='port')
+        dest='port',
+        required=True)
 
 parser.add_argument(
         '--username', '-u',
         action='store',
         help='The username to authenticate as',
-        dest='username')
+        dest='username',
+        required=True)
 
 parser.add_argument(
         '--branch', '-b',
         action='store',
-        help='Name of a branch',
-        dest='branch')
+        help='Name of current git branch',
+        dest='branch',
+        required=True)
 
 parser.add_argument(
         '--plan', '-pl',
         action='store',
-        help='Name of plan',
-        dest='plan')
+        help='Name of current bamboo plan',
+        dest='plan',
+        required=True)
+
+args = parser.parse_args()
 
 ssh = SSHClient()
 ssh.set_missing_host_key_policy(AutoAddPolicy())
 ssh.load_system_host_keys()
-
-[args, pass_args] = parser.parse_known_args()
-
 ssh.connect(args.hostname, port=args.port, username=args.username)
 
-with SCPClient(ssh.get_transport()) as scp:
+try:
+    download_artifact(ssh, args.plan, args.branch)
+except SCPException:
+    print ("Artifact of plan {0}, specific for branch {1} not found, "
+           "pulling artifact from branch develop.").format(args.plan, args.branch)
     try:
-        scp.get(os.path.join(ARTIFACTS_DIR, args.plan, args.branch + ARTIFACTS_EXT),
-                local_path=args.plan.replace("-", '_') + ARTIFACTS_EXT)
+        download_artifact(ssh, args.plan)
     except:
-        print "Artifact specific for branch not found, pulling develop"
-        with SCPClient(ssh.get_transport()) as scp:
-            scp.get(os.path.join(ARTIFACTS_DIR, args.plan, DEFAULT_BRANCH + ARTIFACTS_EXT),
-                    local_path=args.plan.replace("-", '_') + ARTIFACTS_EXT)
+        print "Pulling artifact of plan {}, from branch develop failed."\
+            .format(args.plan)
+
+ssh.close()
--- artifacts/push_artifact.py
+++ artifacts/push_artifact.py
@@ -1,5 +1,10 @@
 #! /usr/bin/env python
+"""
+Pushes build artifact to external repo.
+Artifact should be file with extension .tar.gz
 
+Run the script with -h flag to learn about script's running options.
+"""
 import argparse
 import os
 from paramiko import SSHClient, AutoAddPolicy
@@ -7,6 +12,25 @@ from scp import SCPClient
 
 ARTIFACTS_DIR = 'artifacts'
 ARTIFACTS_EXT = '.tar.gz'
+
+
+def upload_artifact(ssh, artifact, plan, branch):
+    """
+    Uploads given artifact to repo.
+    :param ssh: sshclient with opened connection
+    :type ssh: paramiko.SSHClient
+    :param artifact: name of artifact to be pushed
+    :type artifact: str
+    :param plan: name of current bamboo plan
+    :type plan: str
+    :param branch: name of current git branch
+    :type branch: str
+    :return None
+    """
+    # TODO handle simultaneous pulling and pushing
+    with SCPClient(ssh.get_transport()) as scp:
+        scp.put(artifact, remote_path=os.path.join(ARTIFACTS_DIR, plan,
+                                                   branch + ARTIFACTS_EXT))
 
 parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -16,47 +40,56 @@ parser.add_argument(
     '--hostname', '-hn',
     action='store',
     help='Hostname of artifacts repository',
-    dest='hostname')
+    dest='hostname',
+    required=True)
 
 parser.add_argument(
     '--port', '-p',
     action='store',
     type=int,
     help='SSH port to connect to',
-    dest='port')
+    dest='port',
+    required=True)
 
 parser.add_argument(
     '--username', '-u',
     action='store',
     help='The username to authenticate as',
-    dest='username')
+    dest='username',
+    required=True)
 
 parser.add_argument(
     '--artifact', '-a',
     action='store',
-    help='Artifact to push',
-    dest='artifact')
+    help='Artifact to be pushed. It should be file with .tar.gz extension',
+    dest='artifact',
+    required=True)
 
 parser.add_argument(
     '--branch', '-b',
     action='store',
-    help='Name of a branch',
-    dest='branch')
+    help='Name of current git branch',
+    dest='branch',
+    required=True)
 
 parser.add_argument(
     '--plan', '-pl',
     action='store',
-    help='Name of plan',
-    dest='plan')
+    help='Name of current bamboo plan',
+    dest='plan',
+    required=True)
+
+args = parser.parse_args()
 
 ssh = SSHClient()
 ssh.set_missing_host_key_policy(AutoAddPolicy())
 ssh.load_system_host_keys()
-
-[args, pass_args] = parser.parse_known_args()
-
 ssh.connect(args.hostname, port=args.port, username=args.username)
 
-with SCPClient(ssh.get_transport()) as scp:
-    scp.put(args.artifact, remote_path=os.path.join(ARTIFACTS_DIR, args.plan,
-                                                    args.branch + ARTIFACTS_EXT))
+try:
+    upload_artifact(ssh, args.artifact, args.plan, args.branch)
+except:
+    print "Uploading artifact of plan {0}, on branch {1} failed"\
+        .format(args.plan, args.branch)
+
+ssh.close()
