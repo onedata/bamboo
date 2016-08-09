3b059ff964b6c754f938a0fed85d82f183b98af2
Jakub Kudzia <jakub.kudzia@outlook.com>
2016-08-09 11:48:39 +0200

    VFS-2293 - use locks when pulling artifact

--- artifacts/pull_artifact.py
+++ artifacts/pull_artifact.py
@@ -14,10 +14,11 @@ ARTIFACTS_EXT = '.tar.gz'
 DEFAULT_BRANCH = 'develop'
 
 
-def download_artifact(ssh, plan, branch=DEFAULT_BRANCH):
+def download_specific_or_develop(ssh, plan, branch):
     """
-    Downloads artifact from repo. If branch  argument is not passed, downloads
-    artifact from DEFAULT_BRANCH
+    Downloads build artifact for specific plan and branch from repo.
+    If artifact doesn't exist in repo, artifact from default (develop) branch
+    is downloaded.
     :param ssh: sshclient with opened connection
     :type ssh: paramiko.SSHClient
     :param plan: name of current bamboo plan
@@ -26,9 +27,114 @@ def download_artifact(ssh, plan, branch=DEFAULT_BRANCH):
     :type branch: str
     :return None
     """
+    download_artifact_safe(
+            ssh, plan, branch=branch,
+            exception_handler=download_develop_artifact,
+            exception_handler_args=(ssh, plan),
+            exception_log=
+            "Artifact of plan {0}, specific for branch {1} not found"
+            ", pulling artifact from branch develop.".format(plan, branch))
+
+
+def download_develop_artifact(ssh, plan):
+    """
+    Downloads build artifact for specific plan from develop branch.
+    :param ssh: sshclient with opened connection
+    :type ssh: paramiko.SSHClient
+    :param plan: name of current bamboo plan
+    :type plan: str
+    :return None
+    """
+    download_artifact_safe(
+            ssh, plan, DEFAULT_BRANCH,
+            exception_log="Pulling artifact of plan {}, from branch develop "
+                          "failed.".format(plan))
+
+
+def download_artifact_safe(ssh, plan, branch, exception_handler=None, 
+                           exception_handler_args=(), exception_log=""):
+    """
+    Downloads artifact from repo. Locks file while it's being downloaded.
+    If exception is thrown during download, exception_log is printed and
+    exception_handler function is called.
+    :param ssh: sshclient with opened connection
+    :type ssh: paramiko.SSHClient
+    :param plan: name of current bamboo plan
+    :type plan: str
+    :param branch: name of current git branch
+    :type branch: str
+    :param exception_handler: function called when exception is thrown while
+    artifact is being downloaded
+    :type exception_handler: function
+    :param exception_handler_args: args for exception_handler
+    :type exception_handler_args: tuple
+    :param exception_log: log that is printed when exception is thrown while
+    artifact is being downloaded
+    :type exception_log: str
+    :return None
+    """
+    file_name = artifact_path(plan, branch)
+    lock_file(ssh, file_name)
+    try:
+        download_artifact(ssh, plan, branch)
+    except SCPException:
+        print exception_log
+        if exception_handler:
+            exception_handler(*exception_handler_args)
+    finally:
+        unlock_file(ssh, file_name)
+
+
+def download_artifact(ssh, plan, branch):
+    """
+    Downloads artifact from repo via SCP protocol.
+    :param ssh: sshclient with opened connection
+    :type ssh: paramiko.SSHClient
+    :param plan: name of current bamboo plan
+    :type plan: str
+    :param branch: name of current git branch
+    :type branch: str
+    :return None
+    """
+    path = artifact_path(plan, branch)
     with SCPClient(ssh.get_transport()) as scp:
-        scp.get(os.path.join(ARTIFACTS_DIR, plan, branch + ARTIFACTS_EXT),
-                local_path=args.plan.replace("-", '_') + ARTIFACTS_EXT)
+        scp.get(path, local_path=plan.replace("-", '_') + ARTIFACTS_EXT)
+
+
+def lock_file(ssh, file_name):
+    """
+    Set lock on file_name via ssh. Hangs if file_name is currently locked.
+    :param ssh: sshclient with opened connection
+    :type ssh: paramiko.SSHClient
+    :param file_name: name of file to be locked
+    :type file_name: str
+    :return None
+    """
+    ssh.exec_command("lockfile {}.lock".format(file_name))
+
+
+def unlock_file(ssh, file_name):
+    """
+    Delete lock on file_name via ssh.
+    :param ssh: sshclient with opened connection
+    :type ssh: paramiko.SSHClient
+    :param file_name: name of file to be unlocked
+    :type file_name: str
+    :return None
+    """
+    ssh.exec_command("rm -rf {}.lock".format(file_name))
+
+
+def artifact_path(plan, branch):
+    """
+    Returns path to artifact for specific plan and branch. Path is relative
+    to user's home directory on repository machine.
+    :param plan: name of current bamboo plan
+    :type plan: str
+    :param branch: name of current git branch
+    :type branch: str
+    """
+    return os.path.join(ARTIFACTS_DIR, plan, branch + ARTIFACTS_EXT)
 
 
 parser = argparse.ArgumentParser(
@@ -78,15 +184,6 @@ ssh.set_missing_host_key_policy(AutoAddPolicy())
 ssh.load_system_host_keys()
 ssh.connect(args.hostname, port=args.port, username=args.username)
 
-try:
-    download_artifact(ssh, args.plan, args.branch)
-except SCPException:
-    print ("Artifact of plan {0}, specific for branch {1} not found, "
-           "pulling artifact from branch develop.").format(args.plan, args.branch)
-    try:
-        download_artifact(ssh, args.plan)
-    except:
-        print "Pulling artifact of plan {}, from branch develop failed."\
-            .format(args.plan)
+download_specific_or_develop(ssh, args.plan, args.branch)
 
 ssh.close()
