4760cca4385ef7bd082985345d339cd85fbd41e2
Wojciech Geisler <wojciech.geisler@gmail.com>
2017-11-28 13:14:48 +0100

    VFS-3614 Generate a legal domain for provider

--- docker/getting_started_env_up.py
+++ docker/getting_started_env_up.py
@@ -180,7 +180,7 @@ def modify_provider_docker_compose(provider_name, base_file_str,
     file_str = re.sub(r'(cluster:\s*domainName: ).*',
                       r'\1"{}"'.format(provider), file_str)
     file_str = re.sub(r'domain: .*',
-                      r'domain: "node1.{}"'.format(
+                      r'domain: "node1.{}.dev"'.format(
                           provider_name), file_str)
     with open(provider_dockerfile, "w") as f:
         f.write(file_str)
