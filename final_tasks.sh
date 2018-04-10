8741a9b16625c04eee846991a49f4dea6bfa26d5
plgcwiertni <cwiertnia.michal@gmail.com>
2018-04-10 16:06:17 +0200

    VFS-4306 Fixed removing spaces data

--- final_tasks.sh
+++ final_tasks.sh
@@ -6,6 +6,11 @@ CONTAINERS_TO_REMOVE=${CONTAINERS}
 ONEDATA_STORAGE_PATH="/tmp/onedata"
 
 
+# clear spaces data
+echo "Clearing ${ONEDATA_STORAGE_PATH}"
+docker run -v ${ONEDATA_STORAGE_PATH}:${ONEDATA_STORAGE_PATH} ubuntu bash -c "rm -rf ${ONEDATA_STORAGE_PATH}/*"
+
+
 for container in ${CONTAINERS}
 do
     if [ $(docker inspect --format "{{ index .Config.Labels \"io.kubernetes.container.name\"}}" ${container}) ]
@@ -25,8 +30,3 @@ done
 
 echo "Removing stalled docker volumes"
 docker volume rm $(docker volume ls -q)
-
-
-# clear spaces data
-echo "Clearing ${ONEDATA_STORAGE_PATH}"
-rm -rf ${ONEDATA_STORAGE_PATH}/*
