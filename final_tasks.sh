5fca000d38e5b96bbd340226d688e7eb579a353a
plgcwiertni <cwiertnia.michal@gmail.com>
2018-04-11 11:46:09 +0200

    VFS-4306 Fixed removing files in /tmp/onedata

--- final_tasks.sh
+++ final_tasks.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-K8S_CONTAINER_NAME_LABEL_KEY="io.kubernetes.container.name"
-CONTAINERS=$(docker ps -qa)
-CONTAINERS_TO_REMOVE=${CONTAINERS}
 ONEDATA_STORAGE_PATH="/tmp/onedata"
-
 
 # clear spaces data
 echo "Clearing ${ONEDATA_STORAGE_PATH}"
-docker run -v ${ONEDATA_STORAGE_PATH}:${ONEDATA_STORAGE_PATH} ubuntu bash -c "rm -rf ${ONEDATA_STORAGE_PATH}/*"
+docker run -v ${ONEDATA_STORAGE_PATH}:${ONEDATA_STORAGE_PATH} alpine sh -c "rm -rf ${ONEDATA_STORAGE_PATH}/*"
 
+
+K8S_CONTAINER_NAME_LABEL_KEY="io.kubernetes.container.name"
+CONTAINERS=$(docker ps -qa)
+CONTAINERS_TO_REMOVE=${CONTAINERS}
 
 for container in ${CONTAINERS}
 do
@@ -21,6 +21,7 @@ done
 
 echo "Stalled docker containers to remove: "
 echo ${CONTAINERS_TO_REMOVE}
+echo "Removing stalled docker containers"
 
 for container in ${CONTAINERS_TO_REMOVE}
 do
@@ -28,5 +29,16 @@ do
     docker rm -fv ${container}
 done
 
+
+STALLED_DOCKER_VOLUMES=$(docker volume ls -q)
+
+echo "Stalled docker volumes to remove: "
+echo ${STALLED_DOCKER_VOLUMES}
 echo "Removing stalled docker volumes"
-docker volume rm $(docker volume ls -q)
+
+for volume in ${STALLED_DOCKER_VOLUMES}
+do
+    docker volume rm ${volume}
+done
+
+echo "Done"
