acb9e0ee6aca290406fc9c15217b5879682bbd78
rwidzisz <rafal.widziszewski@gmail.com>
2023-07-25 19:16:18 +0200

    VFS-11087 Changed grep functions for fixmes and todos

--- scripts/codetag-tracker.sh
+++ scripts/codetag-tracker.sh
@@ -154,11 +154,15 @@ run_grep() {
 
 check_path() {
     FILEPATH=${1}
-    run_grep fixme ${FILEPATH} >> ${OUTPUT_FILE}
+    run_grep 'fixme\s' ${FILEPATH} >> ${OUTPUT_FILE}
+    run_grep 'fixme$' ${FILEPATH} >> ${OUTPUT_FILE}
+    run_grep 'fixme:' ${FILEPATH} >> ${OUTPUT_FILE}
     run_grep 'writeme\s'  ${FILEPATH} >> ${OUTPUT_FILE}
     run_grep 'writeme$'  ${FILEPATH} >> ${OUTPUT_FILE}
     run_grep 'writeme:'  ${FILEPATH} >> ${OUTPUT_FILE}
-    run_grep todo ${FILEPATH} | sed -E '/VFS-[0-9]+/d' >> ${OUTPUT_FILE}
+    run_grep  'todo\s' ${FILEPATH} | sed -E '/VFS-[0-9]+/d' >> ${OUTPUT_FILE}
+    run_grep  'todo$' ${FILEPATH} | sed -E '/VFS-[0-9]+/d' >> ${OUTPUT_FILE}
+    run_grep  'todo:' ${FILEPATH} | sed -E '/VFS-[0-9]+/d' >> ${OUTPUT_FILE}
     run_grep 'rpc:multicall' ${FILEPATH} >> ${OUTPUT_FILE}
     if [ -n "${VFS_TAG}" ]; then
         run_grep ${VFS_TAG} ${FILEPATH} >> ${OUTPUT_FILE}
